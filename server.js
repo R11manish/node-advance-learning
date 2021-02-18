e8afa7ff1f9682986ad7986d6ffcb79d63b0f724
Manish Rawat <github.jmtss@gmail.com>
2021-02-17 23:43:46 -0800

    api create

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const  fs  = require('fs');
 const http = require('http');
 const url = require('url');
+
+const data = fs.readFileSync(`${__dirname}/data.json`,'utf-8');
+const dataObject = JSON.parse(data);
 
 const server = http.createServer((req,res)=>{
     const urlPath = req.url;
@@ -8,7 +12,13 @@ const server = http.createServer((req,res)=>{
         res.end('hey whatsup buddy finally we founded that been amzing journey');
     } else  if(urlPath === '/exit'){
         res.end('Hopes that you will come back to us .it been amazing journey withg uh all');
-    } else {
+    } else if(urlPath === '/api') {
+        res.writeHead(200 , {
+            'content-type' : 'application/json'
+        });
+        res.end(data);
+    } 
+     else {
         res.writeHead(404,{
             'content-type' : 'text/html',
             'security-level' :'very-very-high'
