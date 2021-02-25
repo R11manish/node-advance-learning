c44c2e14b16b8e0e83692d7756abdd6dcb1d61a6
Manish Rawat <github.jmtss@gmail.com>
2021-02-24 20:21:20 -0800

    fix sync to async

--- testing/event-loop.js
+++ testing/event-loop.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 4;
 
 setTimeout(() => console.log("timer 1 finished"), 0);
 
@@ -8,9 +12,24 @@ console.log("hello from the top level of code");
 
 fs.readFile("test-file.txt", () => {
   console.log("i/o finished");
-  console.log('--------------------------------------')
+  console.log("--------------------------------------");
   setTimeout(() => console.log("timer 2 finished"), 0);
   setTimeout(() => console.log("timer 3 finished"), 3000);
   setImmediate(() => console.log("immediate 2 finished"));
-  process.nextTick(()=>console.log('process.nextTickExecuted()'));
+  process.nextTick(() => console.log("process.nextTickExecuted()"));
+
+  // 4 thread pool working together
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "password encrypted");
+  });
 });
